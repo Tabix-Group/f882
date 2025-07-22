51d755d949f548373c2fdf5398ea0f48985951d2
hernanofx <hernandorra@gmail.com>
2025-07-22 11:25:39 -0300

    7

--- frontend/src/reportWebVitals.js
+++ frontend/src/reportWebVitals.js
@@ -1,13 +1,3 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
 // Modern React-compatible reportWebVitals.js
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
@@ -22,4 +12,3 @@ const reportWebVitals = onPerfEntry => {
 };
 
 export default reportWebVitals;
-// ...existing code...
