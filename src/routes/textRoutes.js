c9cc08ba308b56e03032b025fa1e5de9f53842a6
hernanofx <hernandorra@gmail.com>
2025-10-11 22:41:43 -0300

    removidos js

--- src/routes/textRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const textController_1 = require("../controllers/textController");
-const router = (0, express_1.Router)();
-router.get('/', textController_1.getTexts);
-exports.default = router;
